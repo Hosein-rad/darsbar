4290dea5dc730b4e47938867c4c52e762286d926
hosein_rad <h.devil1377@gmail.com>
2025-09-30 18:44:02 +0330

    added connection to Prisma DB

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,14 +1,13 @@
 import { PrismaClient } from '@prisma/client'
+import { withAccelerate } from '@prisma/extension-accelerate'
+
+const prismaSingleton = new PrismaClient().$extends(withAccelerate())
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
+  prisma?: typeof prismaSingleton
 }
 
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ['query'],
-  })
+export const prisma = globalForPrisma.prisma ?? prismaSingleton
 
 if (process.env.NODE_ENV !== 'production') {
   globalForPrisma.prisma = prisma
